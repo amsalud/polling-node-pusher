df75eff76571c6255819a1dfac708ea14f251cb0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-15 04:52:27 -0400

    Add fetch method to fetch latest votes

--- public/main.js
+++ public/main.js
@@ -18,44 +18,58 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 });
 
-let dataPoints = [
-  { label: 'Windows', y: 0 },
-  { label: 'MacOs', y: 0 },
-  { label: 'Linux', y: 0 },
-  { label: 'Other', y: 0 }
-];
+fetch('http://localhost:3000/poll')
+  .then(res => res.json())
+  .then(data => {
+    const votes = data.votes;
+    const totalVotes = votes.length;
+    // Count vote points
+    const voteCounts = votes.reduce(
+      (acc, vote) => (
+        (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc
+      ),
+      {}
+    );
 
-const chartContainer = document.querySelector('#chartContainer');
+    let dataPoints = [
+      { label: 'Windows', y: voteCounts['Windows'] },
+      { label: 'MacOs', y: voteCounts['MacOs'] },
+      { label: 'Linux', y: voteCounts['Linux'] },
+      { label: 'Other', y: voteCounts['Other'] }
+    ];
 
-if (chartContainer) {
-  const chart = new CanvasJS.Chart('chartContainer', {
-    animationEnabled: true,
-    theme: 'theme1',
-    title: {
-      text: 'OS Results'
-    },
-    data: [{ type: 'column', dataPoints: dataPoints }]
+    const chartContainer = document.querySelector('#chartContainer');
+
+    if (chartContainer) {
+      const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'theme1',
+        title: {
+          text: `Total Votes ${totalVotes}`
+        },
+        data: [{ type: 'column', dataPoints: dataPoints }]
+      });
+      chart.render();
+
+      // Enable pusher logging - don't include this in production
+      Pusher.logToConsole = true;
+
+      var pusher = new Pusher('bacf49254b65677e71f3', {
+        cluster: 'us2',
+        forceTLS: true
+      });
+
+      var channel = pusher.subscribe('os-poll');
+      channel.bind('os-vote', function(data) {
+        dataPoints.map(x => {
+          if (x.label == data.os) {
+            x.y += data.points;
+            return x;
+          } else {
+            return x;
+          }
+        });
+        chart.render();
+      });
+    }
   });
-  chart.render();
-
-  // Enable pusher logging - don't include this in production
-  Pusher.logToConsole = true;
-
-  var pusher = new Pusher('bacf49254b65677e71f3', {
-    cluster: 'us2',
-    forceTLS: true
-  });
-
-  var channel = pusher.subscribe('os-poll');
-  channel.bind('os-vote', function(data) {
-    dataPoints.map(x => {
-      if (x.label == data.os) {
-        x.y += data.points;
-        return x;
-      } else {
-        return x;
-      }
-    });
-    chart.render();
-  });
-}
