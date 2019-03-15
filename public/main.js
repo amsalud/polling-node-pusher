d40fe280a4e0386888e8dd48700dd040f1a2e58e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-14 23:32:58 -0400

    Add real time update to poll chart

--- public/main.js
+++ public/main.js
@@ -17,3 +17,45 @@ form.addEventListener('submit', e => {
     .catch(err => console.log(err));
   e.preventDefault();
 });
+
+let dataPoints = [
+  { label: 'Windows', y: 0 },
+  { label: 'MacOs', y: 0 },
+  { label: 'Linux Distro', y: 0 },
+  { label: 'Other', y: 0 }
+];
+
+const chartContainer = document.querySelector('#chartContainer');
+
+if (chartContainer) {
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'theme1',
+    title: {
+      text: 'OS Results'
+    },
+    data: [{ type: 'column', dataPoints: dataPoints }]
+  });
+  chart.render();
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher('bacf49254b65677e71f3', {
+    cluster: 'us2',
+    forceTLS: true
+  });
+
+  var channel = pusher.subscribe('os-poll');
+  channel.bind('os-vote', function(data) {
+    dataPoints.map(x => {
+      if (x.label == data.os) {
+        x.y += data.points;
+        return x;
+      } else {
+        return x;
+      }
+    });
+    chart.render();
+  });
+}
